a3d62528e91f061b51596e9a43e9ed108b299cd1
pierre <pierre>
2022-05-08 16:12:12 +0200

    egui/eframe template

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+#![warn(clippy::all, rust_2018_idioms)]
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
+
 use export::make_program;
 use std::env;
 use std::fs::File;
@@ -11,11 +14,22 @@ mod keygroup;
 mod parse;
 mod range;
 
+#[cfg(not(target_arch = "wasm32"))]
 fn main() {
+    // Log to stdout (if you run with `RUST_LOG=debug`).
+    tracing_subscriber::fmt::init();
+
     let args: Vec<String> = env::args().collect();
 
     if args.len() == 2 {
         cli(&args[1]);
+    } else {
+        let native_options = eframe::NativeOptions::default();
+        eframe::run_native(
+            "eframe template",
+            native_options,
+            Box::new(|cc| Box::new(mpc_keygroup_creator::TemplateApp::new(cc))),
+        );
     }
 }
 
